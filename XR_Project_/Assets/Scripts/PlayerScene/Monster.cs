47dca974bdd88cc1d51ea4de9b7844a4c4aa43fc
shingugitvr000 <shingugitvr000@gmail.com>
2023-04-12 11:08:07 +0900

    캐릭터 총알 발사까지

---
+++ XR_Project_/Assets/Scripts/PlayerScene/Monster.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    
+    public int Hp = 5;
+
+    public void Damaged(int Damage)
+    {
+        Hp -= Damage;
+        if (Hp <= 0)
+        {
+            GameObject temp = this.gameObject;
+            Destroy(temp);
+        }
+    }
+}
---
+++ XR_Project_/Assets/Scripts/PlayerScene/ProjectileMove.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    public Vector3 launchDirection;                             //발사방향
+
+    private void FixedUpdate()
+    {
+        float moveAmout = 10 * Time.fixedDeltaTime;              //이동 속도 설정
+        transform.Translate(launchDirection * moveAmout);       //Translate로 이동
+    }
+
+    private void OnCollisionEnter(Collision collision)          //충돌이 일어났을 경우
+    {
+        Debug.Log(collision.gameObject.name);
+        if(collision.gameObject.tag == "Object")                //Tag 값이 오브젝트인 경우
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Monster")                //Tag 값이 오브젝트인 경우
+        {
+            Destroy(this.gameObject);
+            collision.gameObject.GetComponent<Monster>().Damaged(1);
+        }
+    }
+
+}
+
+
+//Projectilemove
