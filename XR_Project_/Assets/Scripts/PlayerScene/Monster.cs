bbb57279c7e3b305e5dcb40f1db605aff4482022
shingugitvr000 <shingugitvr000@gmail.com>
2023-05-03 20:12:46 +0900

    FxManager Update

--- XR_Project_/Assets/Scripts/PlayerScene/Monster.cs
+++ XR_Project_/Assets/Scripts/PlayerScene/Monster.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Monster : MonoBehaviour
-{   
+{
+    protected FxManager FxManager => FxManager.Instance;
     public int Hp = 5;
     
     public void Damaged(int Damage)
@@ -13,6 +14,8 @@ public class Monster : MonoBehaviour
         {
             GameObject temp = this.gameObject;
             Destroy(temp);
+            FxManager.PlayFx(this.gameObject.transform, FxType.Explosion, new Vector3(0.0f, 1.0f ,0.0f));
+
         }
     }
 }
--- XR_Project_/Assets/Scripts/PlayerScene/ProjectileMove.cs
+++ XR_Project_/Assets/Scripts/PlayerScene/ProjectileMove.cs
@@ -14,6 +14,8 @@ public class ProjectileMove : MonoBehaviour
 
     public PROJECTILETYPE projectileType;
 
+    protected FxManager FxManager => FxManager.Instance;
+
     private void FixedUpdate()
     {
         float moveAmount = 10 * Time.fixedDeltaTime;             //이동 속도 설정
@@ -41,6 +43,9 @@ public class ProjectileMove : MonoBehaviour
             GameObject Temp = GameObject.FindGameObjectWithTag("GameManager");
             Temp.GetComponent<HUDTextManager>().UpdateHUDTextSet(
                 "1", other.gameObject, new Vector3(0.0f, 10.0f, 0.0f));
+
+            FxManager.PlayFx(this.gameObject.transform, FxType.hit, Vector3.zero);
+
         }
 
         if (other.CompareTag("Player") && projectileType == PROJECTILETYPE.MONSTER)   //Tag 를 검사한다.
@@ -50,6 +55,8 @@ public class ProjectileMove : MonoBehaviour
             GameObject Temp = GameObject.FindGameObjectWithTag("GameManager");
             Temp.GetComponent<HUDTextManager>().UpdateHUDTextSet(
                 "1", other.gameObject, new Vector3(0.0f, 10.0f, 0.0f));
+
+            FxManager.PlayFx(this.gameObject.transform, FxType.hit, Vector3.zero);
         }
     }
 }
