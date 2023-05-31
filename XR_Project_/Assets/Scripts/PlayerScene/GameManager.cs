425662dca369b99ef0ec5141c3c6e02455f4fe16
shingugitvr000 <shingugitvr000@gmail.com>
2023-05-31 19:35:35 +0900

    코드 업데이트

--- XR_Project_/Assets/Scripts/PlayerScene/GameManager.cs
+++ XR_Project_/Assets/Scripts/PlayerScene/GameManager.cs
@@ -2,21 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;               // UI 추가 
+using System;                       // 추가 
 
 public class GameManager : MonoBehaviour
-{    public GameManager() { }
+{
+    public enum GameState           //게임 상태값 설정
+    {
+        Start,
+        Playing,
+        GameOver
+    }
+
+    public event Action<GameState> OnGameStateChanged;
+
+    public GameState currentState = GameState.Start;
+
+    public GameState CurrentState
+    {
+        get { return currentState; }
+        private set
+        {
+            currentState = value;
+            OnGameStateChanged?.Invoke(currentState);       //이벤트가 null이 아닌경우에만 이 이벤트를 호출 
+        }
+    }
+
+    public void StartGame()
+    {   //게임 시작 로직을 여기에 작성
+        CurrentState = GameState.Playing;
+    }
+
+    public void GameOver()
+    {   //게임 오버 로직을 여기에 작성
+        CurrentState = GameState.GameOver;
+    }
+
+    public GameManager() { }
     public static GameManager Instance { get; private set; }    //싱글톤화
     public PlayerHp playerHp;                   //플레이어의 Hp
     public Image playerHpUIImage;               //플레이어 Hp UI 이미지
     public Button BtnSample;                    //UI 버튼 선언
-
-    private void Start()
-    {
-        this.BtnSample.onClick.AddListener(() =>        //Listenr 로 버튼 등록 
-        {
-            Debug.Log("Button Check");
-        });
-    }
 
     private void Awake()
     {
